ce5e459c099293a74286cca8f4e4139ff7314bd3
Nsity <bisivanovo@yandex.ru>
2018-02-16 23:54:46 +0300

    Подвал

--- footer.php
+++ footer.php
@@ -13,21 +13,27 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <?php get_sidebar( 'footerfull' ); ?>
 
-<div class="wrapper" id="wrapper-footer">
+<div class="big-wrapper" id="wrapper-footer">
 
 	<div class="<?php echo esc_attr( $container ); ?>">
 
 		<div class="row">
 
-			<div class="col-md-12">
+			<div class="col-12 col-md-8">
 
 				<footer class="site-footer" id="colophon">
 
-					&copy; 2018 Designed by Nsity
+					&copy; 2018 Designed by Nsity					
 
 				</footer><!-- #colophon -->
 
 			</div><!--col end -->
+
+			<div class="col-12 col-md-4">
+
+				
+
+			</div>
 
 		</div><!-- row end -->
 
